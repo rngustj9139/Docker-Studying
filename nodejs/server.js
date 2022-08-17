96ffe70075acf9ef3ad2585cc0367fbedb967276
rngustj9139 <mnb9139@khu.ac.kr>
2022-08-18 07:15:58 +0900

    Add 1, nodejs

---
+++ nodejs/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const PORT = 8080;
+const HOST = "localhost";
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("Hello, World!");
+})
+
+app.listen(PORT, HOST);
+
+console.log('server is running')
+

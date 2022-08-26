656e150279a4b79b694afabae4f7e95bb11634f3
rngustj9139 <mnb9139@khu.ac.kr>
2022-08-26 13:49:47 +0900

    Add nodejs4, nodejs5

---
+++ nodejs5/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const redis = require("redis");
+
+const client = redis.createClient({ // 레디스 클라이언트 생성
+    socket: {
+        host: "redis-server", // 도커 환경에서는 redis-server를 추가하고(docker-compose.yml파일에 명시한 컨테이너 이름이다.) redis서버가 작동하는 곳이 redis-server.com이라면 https://redis-server.com을 추가한다.
+        port: 6379 // 레디스의 기본 포트는 6379이다.
+    }
+});
+
+const app = express();
+app.get('/', async (req, res) => {
+    await client.connect();
+    let number = await client.get('number');
+    if (number === null) {
+        number = 0;
+    }
+
+    console.log('Number: ' + number);
+    res.send("숫자가 1씩 올라갑니다. 숫자: " + number)
+
+    await client.set("number", parseInt(number) + 1) // "number"는 key고 parseInt속 number는 value이다.
+    await client.disconnect();
+})
+
+app.listen(8080);
+
+console.log('Server is running');
+

8d491014dbd28955f46ad72de6b8237cb15bf976
rngustj9139 <mnb9139@khu.ac.kr>
2022-09-02 10:51:25 +0900

    Update part6

--- part6/src/App.js
+++ part6/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello, World!222
         </a>
       </header>
     </div>
